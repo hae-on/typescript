56b86b5be13251de90f28bf798ec503e63872760
hae-on <solbi2004@naver.com>
2022-01-18 16:57:27 +0900

    try, catch, finally

---
+++ 8-exception/8-1-exception.ts
@@ -0,0 +1,22 @@
+// Error(Exception) Handling: try -> catch -> finally
+function readFile(fileName: string): string {
+  if (fileName === "not exist!💩") {
+    throw new Error(`file not exist! ${fileName}`);
+  }
+  return "file contents";
+}
+
+function closeFile(fileName: string) {}
+
+const fileName = "not exist!💩";
+
+try {
+  console.log(readFile(fileName));
+} catch (error) {
+  console.log(`catched!!!`);
+} finally {
+  closeFile(fileName);
+  console.log(`finally!`);
+}
+
+console.log(`!!!`);
