60b328ca1193b47963125f8771e5b3b8195d8e54
hae-on <solbi2004@naver.com>
2022-01-21 17:11:55 +0900

    video

--- motion/dist/app.js
+++ motion/dist/app.js
@@ -1,3 +1,4 @@
+import { VideoComponent } from "./components/page/item/video.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
@@ -8,6 +9,8 @@ class App {
         this.page.attachTo(appRoot);
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
         image.attachTo(appRoot, "beforeend");
+        const video = new VideoComponent("Video Title", "https://youtu.be/K3-jG52XwuQ");
+        video.attachTo(appRoot, "beforeend");
         const note = new NoteComponent("Note Title", "Note Body");
         note.attachTo(appRoot, "beforeend");
         const todo = new TodoComponent("Todo Title", "Todo Item");
